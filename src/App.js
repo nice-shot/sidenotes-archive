5578cec6affe79cd617f48adbc4ac766a34116c9
nice-shot <shefferu@gmail.com>
2019-01-07 18:12:17 +0200

    Removed unused import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import path from 'path'
 
 import ImageViewer from './components/ImageViewer'
 
 class App extends Component {
-  
+
   render() {
     const imagePath = 'file://E:/Programming/sidenotes-archive/tests/UnderstandingAnimation_20161120_P-002.jpg'
     console.log(imagePath)
